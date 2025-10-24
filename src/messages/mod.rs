b1bc5b5f4ec57a754cbf80d56089645478a1bc26
Daniel Bucher <DanielC.B@web.de>
2025-10-24 11:14:50 +0200

    Moved dep. for messages to lotus-extra

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -1,7 +1,7 @@
 pub mod bus_messages;
 pub mod coupling_handler;
 pub mod diagnostic_messages;
-pub mod fis_messages;
+//pub mod fis_messages;
 pub mod gt6n_coupling_messages;
 pub mod pandemist_messages;
-pub mod std_messages;
+//pub mod std_messages;
